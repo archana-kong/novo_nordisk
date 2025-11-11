7760e9cf0af8a22bf450b65db5fe4bda1f0c2dcf
archana-kong <archana.km@konghq.com>
2025-11-11 22:13:12 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "server_url" {
+    description = "server_url"
+        type = string
+}
+variable "system_account_access_token" {
+    description = "server_url"
+        type = string
+}
+variable "environment" {
+    description = "server_url"
+        type = string
+}
