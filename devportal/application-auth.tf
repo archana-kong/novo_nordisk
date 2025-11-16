5b7550bb7a63cb793a110a0a76bf4b79c82179e8
archana-kong <archana.km@konghq.com>
2025-11-16 22:24:02 +0530

    Update application-auth.tf

--- devportal/application-auth.tf
+++ devportal/application-auth.tf
@@ -2,17 +2,28 @@
 
 
 #Create Auth stratergy
-resource "konnect_application_auth_strategy" "azure_oidc" {
+resource "konnect_application_auth_strategy" "azure-oidc-dcr" {
   name           = "Azure OIDC"
-  display_name   = "Azure OIDC"
-  strategy_type  = "openid_connect"
-  dcr_provider_id = "6bd15c90-acac-4599-82ac-e442d1f73a47"
-  configs = {
-    "openid-connect" = {
-      issuer           = "https://sts.windows.net/f177c1d6-50cf-49e0-818a-a0585cbafd8d"
-      credential_claim = ["appid"]
-      auth_methods     = ["client_credentials", "bearer", "session"]
-      scopes         = ["openid", "email"] # Optional, add as needed
+  display_name = "Azure OIDC"
+  active = true
+  openid_connect = {
+    active = true
+    dcr_provider_id = "6bd15c90-acac-4599-82ac-e442d1f73a47"
+    display_name = "Azure OIDC + DCR Strategy"
+    configs = {
+      openid_connect = {
+        issuer           = "https://sts.windows.net/f177c1d6-50cf-49e0-818a-a0585cbafd8d"
+        auth_methods     = ["client_credentials", "bearer", "session"]
+        scopes            = ["openid", "email"]
+        credential_claim = ["appid"]
+      }
     }
+    dcr_provider = {
+      id           = "6bd15c90-acac-4599-82ac-e442d1f73a47"
+      name         = "azure-dcr-provider"
+      display_name = "Azure Dynamic Client Registration"
+      provider_type = "azureAd"
+    }
+   
   }
 }
